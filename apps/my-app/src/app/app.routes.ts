9d0e27655ddf6a42b7b9b7af1503dc2fc5833eae
RStruijlaart <79460280+RStruijlaart@users.noreply.github.com>
2024-11-13 20:59:07 +0100

    added routing

--- apps/my-app/src/app/app.routes.ts
+++ apps/my-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserDetailsComponent } from 'libs/frontend/features/src/lib/users/user-details/user-details.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: "", pathMatch: "full", redirectTo: "dashboard" },
+    { path: "dashboard", component: DashboardComponent },
+    { path: "about", component: AboutComponent },
+    { path: "user-details", pathMatch: "full", component: UserDetailsComponent },
+    // Catch-all route: als er geen URL match is gaan we naar component-a (of dashboard, of naar 404)
+    { path: "**", redirectTo: "dashboard" },
+];
