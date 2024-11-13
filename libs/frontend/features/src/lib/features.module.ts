05badcd3d2ffc29c0d1d0dd150c9a8722db4e5ee
RSruijlaart <r@struijlaart.nl>
2024-11-13 14:56:47 +0100

    added some libs

---
+++ libs/frontend/features/src/lib/features.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [UserDetailsComponent]
+})
+export class FeaturesModule {}
